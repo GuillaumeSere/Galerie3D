5d268e30646a43633bbc29afd1a322a1eada42aa
Guillaume Sere <guillaumesere@hotmail.fr>
2023-09-07 16:05:50 +0200

    add rotation

--- script.js
+++ script.js
@@ -3,6 +3,18 @@ let next = document.querySelector('.next');
 let box = document.querySelector('.box');
 
 let degrees = 0;
+let rotationInterval;
+
+// Fonction pour faire tourner les images automatiquement
+function startRotation() {
+    rotationInterval = setInterval(function() {
+        degrees += 45;
+        box.style.transform = `perspective(1000px) rotateY(${degrees}deg)`;
+    }, 2000); // Réglez l'intervalle de rotation ici (2 secondes dans cet exemple)
+}
+
+// Démarrez la rotation automatique au chargement de la page
+startRotation();
 
 prev.addEventListener('click', function(){
     degrees += 45;
